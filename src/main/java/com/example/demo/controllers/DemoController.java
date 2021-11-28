97cd4dd4a458cf4ec474a34fdb611ca714e892a2
Kshitija96 <kshitija96.kul@gmail.com>
2021-11-28 15:24:34 +0530

    change url

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-	@GetMapping("/gg")
+	@GetMapping("java-base-git-kshitija96-kul-dev.apps.sandbox.x8i5.p1.openshiftapps.com/gg")
 	public String greeting() {
 		return "home";
 	}
